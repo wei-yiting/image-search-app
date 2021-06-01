e529c80e67c3d54fa3cbb006dc87c5737d640b05
WYT <tina.yiting.wei@gmail.com>
2021-06-01 16:32:43 +0800

    build ImageList component

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
+
+const ImageList = ({ images }) => {
+  const imageElements = images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+  return <div className="image-list">{imageElements}</div>;
+};
+
+export default ImageList;
